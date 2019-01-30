3c333c88d40e8fa3e6f4aeb1fb5d2e31acb18474
dong <735050289@qq.com>
2019-01-30 22:41:17 +0800

    添加axios,信息列表

--- api/index.js
+++ api/index.js
@@ -1,8 +1,7 @@
 import axios from './axios'
-import {URL} from '../api/variable'
 
 // 获取首页信息展示
-export function getInformationByType(type, pageSize = 5, currentPage = 1) {
+export function getInformationByType(type, pageSize, currentPage) {
   if (type === 'cultivation' || type === 'knowledge') {
     pageSize = 10
   }
@@ -12,9 +11,18 @@ export function getInformationByType(type, pageSize = 5, currentPage = 1) {
     type
   }
   return axios({
-    url: `${URL}getInformationByType.action`,
+    url: `getInformationByType.action`,
     method: 'post',
     data: 'inputjson=' + JSON.stringify(inputjson),
+    withCredentials: true
+  })
+}
+
+export function readInformation(id) {
+  return axios({
+    url: `readInformation.action`,
+    data: 'inputjson=' + id,
+    method: 'post',
     withCredentials: true
   })
 }
